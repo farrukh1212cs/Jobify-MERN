e13244c2d3d1f53bca1276b9dcd5aea046b75617
Farrukh Rehman <farrukh1212cs@outlook.com>
2023-05-10 19:21:06 +0500

    Register Page, Alert, FormRow, Toggle

---
+++ client/src/components/Alert.js
@@ -0,0 +1,10 @@
+// import { useAppContext } from '../context/appContext'
+
+const Alert = () => {
+//   const { alertType, alertText } = useAppContext()
+//   return <div className={`alert alert-${alertType}`}>{alertText}</div>
+  return <div className="alert alert-danger">here i am</div>
+
+}
+
+export default Alert
--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,7 +1,80 @@
-const Register = () => {
+import { useState, useEffect } from 'react';
+import { Logo,FormRow ,Alert} from '../components';
+import Wrapper from '../assets/wrappers/RegisterPage';
+// global context and useNavigate later
+
+const initialState = {
+  name: '',
+  email: '',
+  password: '',
+  isMember: true,
+  showAlert:false
+};
+// if possible prefer local state
+// global state
+const Register = () =>  {
+  const [values, setValues] = useState(initialState);
+
+  // global context and useNavigate later
+
+  const toggleMember = () =>{
+    setValues({...values,isMember:!values.isMember})
+  }
+  const handleChange = (e) => {
+    console.log(e.target);
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    console.log(e.target);
+  };
   return (
-    <div>Register</div>
-  )
+    <Wrapper className='full-page'>
+      <form className='form' onSubmit={onSubmit}>
+        <Logo />
+        <h3>{values.isMember ? "Login" : 'Register'}</h3>
+        {values.showAlert && <Alert/>}
+
+        {/* name field */}
+        {!values.isMember
+        &&  (<FormRow 
+          type="text"
+          name="name"
+          value={values.name}
+          handleChange={handleChange} 
+            />)
+        
+        }
+        
+         {/* email field */}
+         <FormRow 
+               type="email"
+               name="email"
+               value={values.email}
+               handleChange={handleChange} 
+          />
+         {/* password field */}
+         <FormRow 
+               type="password"
+               name="password"
+               value={values.password}
+               handleChange={handleChange} 
+        />
+
+        <button type='submit' className='btn btn-block'>
+          submit
+        </button>
+        <p>
+          {values.isMember ? 'Not a member yet' : 'Already a member?'}
+          <button type='button' onClick={toggleMember}
+            className='member-btn'>
+              {values.isMember ? 'Register' : 'Login' }
+            </button>
+        </p>
+      </form>
+    </Wrapper>
+  );
 }
 
-export default Register
+
+  export default Register;

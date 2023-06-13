fbd18db1553956d8441548f95eaeb0fa5f94eba3
Farrukh Rehman <50712521+farrukh1212cs@users.noreply.github.com>
2023-06-13 23:41:07 +0500

    44 - Pagination - Server
    001 Pagination - Intro
    002 Page and Limit
    003 Total Jobs and Number of Pages

--- controllers/jobController.js
+++ controllers/jobController.js
@@ -76,7 +76,15 @@ const getAllJobs = async (req,res) =>{
     result = result.sort('-position');
   }
 
+    // setup pagination
+    const page = Number(req.query.page) || 1;
+    const limit = Number(req.query.limit) || 10;
+    const skip = (page - 1) * limit;
+  
+    result = result.skip(skip).limit(limit);
+  
    const jobs =await result
+   
    res.status(StatusCodes.OK).json({jobs,totalJobs:jobs.length,numOfPages:1 })
 }
 

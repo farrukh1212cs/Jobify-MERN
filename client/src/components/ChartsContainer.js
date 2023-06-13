a8debfac9c94fcfa5fba5c73b20cb5cc912d506f
Farrukh Rehman <50712521+farrukh1212cs@users.noreply.github.com>
2023-06-13 22:05:46 +0500

    41 - Monthly Applications - Fron-End
    001 Charts Container
    003 Install Recharts Library
    004 Bar Chart
    005 Area Chart

--- client/src/components/ChartsContainer.js
+++ client/src/components/ChartsContainer.js
@@ -1,9 +1,24 @@
-const ChartsContainer  = () => {
-    return (
-    <div>
-            <h1>Charts Container </h1>
-            
-    </div>)
+import React, { useState } from 'react'
+
+import BarChart from './BarChart'
+import AreaChart from './AreaChart'
+import { useAppContext } from '../context/appContext'
+import Wrapper from '../assets/wrappers/ChartsContainer'
+
+const ChartsContainer = () => {
+  const [barChart, setBarChart] = useState(true)
+
+  const { monthlyApplications: data } = useAppContext()
+  
+  return (
+    <Wrapper>
+      <h4>Monthly Applications</h4>
+      <button type='button' onClick={() => setBarChart(!barChart)}>
+        {barChart ? 'Area Chart' : 'Bar Chart'}
+      </button>
+      {barChart ? <BarChart data={data} /> : <AreaChart data={data} />}
+    </Wrapper>
+  )
 }
 
-export default ChartsContainer 
+export default ChartsContainer
